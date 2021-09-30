5977a3ed188cdb79d14ec33543be043552e554dd
Ruben Suarez B <daricoder@gmail.com>
2021-09-30 13:08:59 -0500

    formulario terminado falta mejorar el atributo value de select multiple

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,33 +2,86 @@ import { useState } from "react";
 import "./Formulario.css";
 
 export const Formulario = () => {
-  const [formData, setFormData] = useState({redes_sociales:[]});
+  const params =[ "nombre","email","edad","sexo","pais","redes_sociales","idiomas"]
+  const [formData, setFormData] = useState({});
   const handleSubmit = (e) => {
     e.preventDefault();
   };
+  const validateFormData = () => {
+    for (const param of params) {
+      if (!formData[param]) {
+        return false
+      }
+    }
+    return true
+  }
 
   const handleChange = (e) => {
-    let { name, value, type } = e.target;
-    console.log(name, value, type, typeof value);
+    let { name, value, type, options } = e.target;
+    // console.log(name, value, type,Object.values(value));
 
-    if (type==="checkbox" && name==="redes_sociales") {
-        if (!formData.redes_sociales.includes(value)) {
-            value = [...formData.redes_sociales, value]
-        }else{
-            value = formData.redes_sociales.filter((red) => red!==value)
+    if (type === "checkbox" && name === "redes_sociales") {
+      if (!formData?.redes_sociales) {
+        formData.redes_sociales = [];
+      }
+      if (!formData.redes_sociales.includes(value)) {
+        value = [...formData.redes_sociales, value];
+      } else {
+        value = formData.redes_sociales.filter((red) => red !== value);
+      }
+      if (!value.length) {
+        value = undefined;
+      }
+    } else if (type === "select-multiple" && name === "idiomas") {
+      if (options) {
+        let values = [];
+        for (var i = 0, l = options.length; i < l; i++) {
+          if (options[i].selected) {
+            values = [...values, options[i].value];
+          }
         }
 
-    } 
+        let idiomas = formData[name];
+        value = [];
+        if (idiomas) {
+          let idiomaEncontrado;
+          for (const idioma of values) {
+            idiomaEncontrado = idiomas.find(
+              (idiomaObj) => idiomaObj.idioma === idioma
+            );
+            if (idiomaEncontrado) {
+              value = [...value, idiomaEncontrado];
+            } else {
+              value = [...value, { idioma: idioma, nivel: 5 }];
+            }
+          }
+        } else {
+          for (const idioma of values) {
+            value = [...value, { idioma: idioma, nivel: 5 }];
+          }
+        }
+        if (!values.length) {
+          value = undefined;
+        }
+      }
+    } else if (type === "range" && name === "nivel") {
+      let idiomaTarget = e.target.getAttribute("data-idioma")
+      let idiomaObj = formData.idiomas.find((idiomaObj)=> idiomaObj.idioma === idiomaTarget);
+      idiomaObj[name]=value;
+      value=undefined;
+
+    }
     setFormData({
       ...formData,
       [name]: type == "number" ? Number(value) : value,
     });
+
   };
 
   const sexos = ["Masculino", "Femenino"];
   const paises = ["USA", "ECUADOR", "CHILE"];
-  const redes_sociales = ["Facebook", "Instagram","Telegra","Whatsapp"];
-
+  const redes_sociales = ["Facebook", "Instagram", "Telegra", "Whatsapp"];
+  const idiomas = ["English", "Español", "Frances", "holandes"];
 
   return (
     <>
@@ -83,43 +136,88 @@ export const Formulario = () => {
           <br />
           <label>
             <span>pais:</span>
-            <select
-              onChange={handleChange}
-              name="paises"
-              value={formData.pais}
-            >
-            
-            <option hidden selected>{formData.pais}</option>
-            {paises.map((pais, i) => (
-              
-            <option key={i} value={pais}>{pais}</option>
-            
-            ))}
-            
+            <select onChange={handleChange} name="pais" value={formData.pais}>
+              <option hidden selected>
+                {formData.pais}
+              </option>
+              {paises.map((pais, i) => (
+                <option key={i} value={pais}>
+                  {pais}
+                </option>
+              ))}
             </select>
           </label>
           <br />
 
           <label>
-            <span>redes sociales:</span>
+            <span>
+              <strong>redes sociales:</strong>
+            </span>
+            <br />
             {redes_sociales.map((red, i) => (
-                <>
-              <label key={i}>
-                <strong>{red}</strong>
-                <input
-                  onChange={handleChange}
-                  name="redes_sociales"
-                  type="checkbox"
-                  value={red}
-                  checked={formData.redes_sociales ? formData.redes_sociales.includes(red): false}
-                />
-              </label>
-              <br />
-              </>
+              <div key={i}>
+                <label key={i}>
+                  <span>{red}</span>
+                  <input
+                    onChange={handleChange}
+                    name="redes_sociales"
+                    type="checkbox"
+                    value={red}
+                    checked={
+                      formData.redes_sociales
+                        ? formData.redes_sociales.includes(red)
+                        : false
+                    }
+                  />
+                </label>
+                <br />
+              </div>
             ))}
           </label>
           <br />
-          <button type="submit">Enviar</button>
+
+          <label>
+            <span>
+              <strong>Idiomas</strong>{" "}
+            </span>
+            <br />
+            <select multiple onChange={handleChange} name="idiomas">
+              {/* <option hidden selected></option> */}
+              {idiomas.map((idioma, i) => (
+                <option key={i} value={idioma}>{idioma}</option>
+              ))}
+            </select>
+          </label>
+          <br />
+          <br />
+          <label>
+            <span>
+              <strong>Eliga un nivel para cada idioma seleccionado</strong>
+            </span>
+            <br />
+            {formData.idiomas &&
+              formData.idiomas.map((idiomaObj,i) => (
+                <div key={i}>
+                  <span>Idioma:{idiomaObj.idioma}</span>
+                  <br />
+                  <span>nivel:{idiomaObj.nivel}</span>
+                  <input
+                    data-idioma={idiomaObj.idioma}
+                    name="nivel"
+                    onChange={handleChange}
+                    value={idiomaObj.nivel}
+                    type="range"
+                    min="1"
+                    max="10"
+                  />
+                  <br />
+                  <br />
+                </div>
+              ))}
+          </label>
+          <br />
+
+          <button disabled={!validateFormData()} type="submit">Enviar</button>
         </form>
         <p>formData: {JSON.stringify(formData)}</p>
       </div>
